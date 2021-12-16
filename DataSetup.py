0c3f00340e062077a7b8d35271f07dd1ad65764d
MicAdes <67414842+MicAdes@users.noreply.github.com>
2021-12-16 15:25:54 +0100

    Delete DataSetup.py

--- DataSetup.py
+++
@@ -1,6 +0,0 @@
-def get_MN40_classes():
-    return ["airplane","bathtub","bed","bench","bookshelf","bottle","bowl","car","chair","cone","cup","curtain","desk","door","dresser","flower_pot","glass_box","guitar","keyboard","lamp","laptop","mantel","monitor","night_stand","person","piano","plant","radio","range_hood","sink","sofa","stairs","stool","table","tent","toilet","tv_stand","vase","wardrobe","xbox"]
-
-def get_MN10_classes():
-    return ["bathtub","bed","chair","desk","dresser","monitor","night_stand","sofa","table","toilet"]
-
